10edf63092bfd0a8d41c7e6769da1e6266a8c342
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-08 15:46:24 +0200

    Building sentences while grabbing data from arrays

---
+++ workingwitharray.js
@@ -0,0 +1,6 @@
+var cat = ["hoefdier", "vis", "vogel"];
+var dier = ["paard", "mus", "goudvis"];
+
+document.getElementById("demo").innerHTML += "de " + dier[0] + " behoort tot de " + cat[0] +"<br>";
+document.getElementById("demo").innerHTML += "de " + dier[1] + " behoort tot de " + cat[2] +"<br>";
+document.getElementById("demo").innerHTML += "de " + dier[2] + " behoort tot de " + cat[1] +"<br>";
