75649f94603faeba909592c8b65749924c415754
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-08 15:42:33 +0200

    Used javascript for capitalising the first letter

---
+++ capitalise.js
@@ -0,0 +1,4 @@
+var myFirstName = "drohn ";
+var lastName = "tewari";
+
+document.getElementById("demo").innerHTML = myFirstName[0].toUpperCase().toUpperCase() + myFirstName.slice(1) + lastName[0].toUpperCase().toUpperCase() + lastName.slice(1);
